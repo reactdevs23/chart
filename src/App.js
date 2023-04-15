3d8f4d6b23f8df6157ff4782c508b45919eb9f5f
dalimkumar604 <dalimkumar212582@gmail.com>
2023-04-15 21:07:46 +0600

    chart

--- src/App.js
+++ src/App.js
@@ -4,68 +4,69 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home";
 import LaunchPad from "./components/LaunchPad/LaunchPad";
 
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 
 import {
   darkTheme,
   getDefaultWallets,
   RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, bsc } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, bsc } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 const hepton = {
   id: 404,
-  name: 'Hepton',
-  network: 'hepton',
-  iconUrl: 'https://pbs.twimg.com/profile_images/1624393158456143874/tdEGrujS_400x400.jpg',
-  iconBackground: '#fff',
+  name: "Hepton",
+  network: "hepton",
+  iconUrl:
+    "https://pbs.twimg.com/profile_images/1624393158456143874/tdEGrujS_400x400.jpg",
+  iconBackground: "#fff",
   nativeCurrency: {
     decimals: 18,
-    name: 'Hepton',
-    symbol: 'HTE',
+    name: "Hepton",
+    symbol: "HTE",
   },
   rpcUrls: {
     default: {
-      http: ['https://testnet.hepton.io'],
+      http: ["https://testnet.hepton.io"],
     },
   },
   blockExplorers: {
-    default: { name: 'SnowTrace', url: 'https://testnet.heptonscan.com' },
+    default: { name: "SnowTrace", url: "https://testnet.heptonscan.com" },
   },
   testnet: true,
 };
 
 const { chains, provider } = configureChains(
   [hepton, bsc],
-  [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  chains
+  appName: "My RainbowKit App",
+  chains,
 });
 
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider
-})
+  provider,
+});
 
 function App() {
   return (
     <>
       <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains} theme={darkTheme()} showRecentTransactions={true}>
+        <RainbowKitProvider
+          chains={chains}
+          theme={darkTheme()}
+          showRecentTransactions={true}
+        >
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/launchpad" element={<LaunchPad />}></Route>
+            <Route path="/" element={<LaunchPad />}></Route>
           </Routes>
           <Footer />
         </RainbowKitProvider>
